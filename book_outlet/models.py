18e60d936f4fbfb2db3736753e07dc1b7a1f251c
yash dhawan <yashdhawan@yashs-MacBook-Pro.local>
2021-12-14 09:11:39 +0530

    Book-Details Merged With DB

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,4 +1,3 @@
-from django.db import models
 from django.core import validators
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
+from django.db.models import Avg,Max,Min
+
+from .models import Book
 
 # Create your views here.
 
 def index(request):
-  return render(request, "book_outlet/index.html")
+  books = Book.objects.all()
+  num_books=books.count()
+  avg_rating=books.aggregate(Avg("rating"))
+  return render(request, "book_outlet/index.html", {
+    "books": books,
+    "total_number_of_books":num_books,
+    "average_rating":avg_rating
+
+  })
+
+
+def book_detail(request, id):
+  # try:
+  #   book = Book.objects.get(pk=id)
+  # except:
+  #   raise Http404()
+  book = get_object_or_404(Book, pk=id)
+  return render(request, "book_outlet/book_detail.html", {
+    "title": book.title,
+    "author": book.author,
+    "rating": book.rating,
+    "is_bestseller": book.is_bestselling
+  })
